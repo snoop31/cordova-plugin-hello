1daca283f2edaacfa51b977c8b1804229ce4692f
snoop31 <snoop31@yandex.ru>
2017-05-29 16:34:48 +0300

    Update Hello.java

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -4,12 +4,53 @@ import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.List;
+import java.lang.reflect.Method;
+import android.util.Log;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.telephony.TelephonyManager;
+import android.content.pm.PackageManager;
+
 public class Hello extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("greet")) {
+            
+        try {
+            // Get the boring old TelephonyManager
+            TelephonyManager telephonyManager =
+                    (TelephonyManager) cordova.getActivity().getSystemService(Context.TELEPHONY_SERVICE);
+
+            // Get the getITelephony() method
+            Class classTelephony = Class.forName(telephonyManager.getClass().getName());
+            Method methodGetITelephony = classTelephony.getDeclaredMethod("getITelephony");
+
+            // Ignore that the method is supposed to be private
+            methodGetITelephony.setAccessible(true);
+
+            // Invoke getITelephony() to get the ITelephony interface
+            Object telephonyInterface = methodGetITelephony.invoke(telephonyManager);
+
+            // Get the endCall method from ITelephony
+            Class telephonyInterfaceClass =  
+                    Class.forName(telephonyInterface.getClass().getName());
+            Method methodEndCall = telephonyInterfaceClass.getDeclaredMethod("endCall");
+
+            // Invoke endCall()
+            methodEndCall.invoke(telephonyInterface);
+
+        } catch (Exception ex) { // Many things can go wrong with reflection calls
+//            Log.d(TAG,"PhoneStateReceiver **" + ex.toString());
+        }
 
             String name = data.getString(0);
             String message = "Hello, " + name;
